a94924406de9969c9be839de50f7068da8d0a3fe
Murembiwa Mutswaletswale <murembiwanoz64@gmail.com>
2025-07-01 18:01:09 +0200

    Update analysis_over_time.sql

--- scripts/analysis_over_time.sql
+++ scripts/analysis_over_time.sql
@@ -1,3 +1,17 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
 /*Analyze the sales performace over time
 --help for trategic reasoning*/
 SELECT
